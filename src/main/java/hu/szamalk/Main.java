d8e745b13229d5230acecb57c8652e8f3bec66e6
bajarigergo <gergob10@gmail.com>
2024-12-09 10:38:03 +0100

    f2 KEZDES

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -8,41 +8,43 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Tabla t = new Tabla("#");
-        System.out.println("4. feladat: az üres tábla:");
-        t.Megjelenit();
 
-        System.out.println("6.feladat: A feltöltött tábla:");
-        t.Elhelyez(8);
-        t.Megjelenit();
 
-        System.out.println("9.feladat: Üres oszlopok és sorok száma:");
-        System.out.println("Oszlopok: "+ t.UresOszlopokSzama());
-        System.out.println("Sorok: " +t.UresSorokSzama());
-
-        String kimenet = "";
-        for (int i = 0; i < 64; i++) {
-            Tabla tabla = new Tabla("*");
-            tabla.Elhelyez(i+1);
-            kimenet += tabla.FajlbaIr() +"\n";
-        }
-        try {
-            File myObj = new File("tablak64.txt");
-            if (myObj.createNewFile()) {
-                FileWriter myWriter = new FileWriter(myObj);
-                myWriter.write(kimenet);
-                myWriter.close();
-            } else {
-                myObj.delete();
-                FileWriter myWriter = new FileWriter(myObj);
-                myWriter.write(kimenet);
-                myWriter.close();
-                /*FileWriter myWriter = new FileWriter(myObj);
-                myWriter.write(kimenet);
-                myWriter.close();*/
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        Tabla t = new Tabla("#");
+//        System.out.println("4. feladat: az üres tábla:");
+//        t.Megjelenit();
+//
+//        System.out.println("6.feladat: A feltöltött tábla:");
+//        t.Elhelyez(8);
+//        t.Megjelenit();
+//
+//        System.out.println("9.feladat: Üres oszlopok és sorok száma:");
+//        System.out.println("Oszlopok: "+ t.UresOszlopokSzama());
+//        System.out.println("Sorok: " +t.UresSorokSzama());
+//
+//        String kimenet = "";
+//        for (int i = 0; i < 64; i++) {
+//            Tabla tabla = new Tabla("*");
+//            tabla.Elhelyez(i+1);
+//            kimenet += tabla.FajlbaIr() +"\n";
+//        }
+//        try {
+//            File myObj = new File("tablak64.txt");
+//            if (myObj.createNewFile()) {
+//                FileWriter myWriter = new FileWriter(myObj);
+//                myWriter.write(kimenet);
+//                myWriter.close();
+//            } else {
+//                myObj.delete();
+//                FileWriter myWriter = new FileWriter(myObj);
+//                myWriter.write(kimenet);
+//                myWriter.close();
+//                /*FileWriter myWriter = new FileWriter(myObj);
+//                myWriter.write(kimenet);
+//                myWriter.close();*/
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
