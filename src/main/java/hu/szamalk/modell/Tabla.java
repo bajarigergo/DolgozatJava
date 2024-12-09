abe6984838b467d0915d95edd3a51a77a14d121e
bajarigergo <gergob10@gmail.com>
2024-12-09 09:18:50 +0100

    f1 1

---
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -0,0 +1,16 @@
+package hu.szamalk.modell;
+
+public class Tabla {
+    private String UresCella;
+    private String[][] T;
+
+    public Tabla() {}
+    private void Elhelyez(){}
+    private void FajlbaIr(){}
+    private void Megjelenit(){}
+    private void UresOszlop(){}
+    private void UresSor(){}
+    private int UresOszlopokSzama(){return 0;}
+    private int UresSorokSzama(){return 0;}
+}
+
