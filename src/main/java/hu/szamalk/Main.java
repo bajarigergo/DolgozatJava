69e238863a3e9e191f94e8927db7a2d48b919018
bajarigergo <gergob10@gmail.com>
2024-12-09 09:39:44 +0100

    f1 6

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -5,6 +5,8 @@ import hu.szamalk.modell.Tabla;
 public class Main {
     public static void main(String[] args) {
         Tabla program = new Tabla("#");
+        program.Elhelyez(8);
+        System.out.println("6. feladat: a feltöltött tábla");
         program.Megjelenit();
     }
 }
